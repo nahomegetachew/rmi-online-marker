762ee861a35070034443d0ce7f89419c8cc91460
nahome getachew <nahomg@gmail.com>
2018-12-09 13:41:00 -0200

    initial commit

---
+++ src/rmi/market/banckAccount.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rmi.market;
+
+/**
+ *
+ * @author wolfe
+ */
+public class banckAccount {
+    int account_No;
+    int password;
+    String username;
+}
